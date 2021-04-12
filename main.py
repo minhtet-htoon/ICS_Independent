cc48866db75e8be88fa728c5e3178ac4e1a2a079
MINHTET HTOON <minhtethtoon@LAHS-COMPSCI18-29.local>
2021-04-12 10:59:48 -0600

    day 1 end

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
 from tensorflow.keras.metrics import categorical_crossentropy
 
 train_labels = []
@@ -40,3 +43,69 @@ model = Sequential([
 # model.summary()
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=True)
+test_labels = []
+test_samples = []
+
+for i in range(50):
+    random_younger = randint(13, 65)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+for i in range(1000):
+    random_younger = randint(13, 65)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+test_labels = np.array(test_labels)
+test_samples = np.array(test_samples)
+test_labels, test_samples = shuffle(test_labels, test_samples)
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1, 1))
+predictions = model.predict(x=scaled_test_samples, batch_size=10, verbose=0)
+rounded_predictions = np.argmax(predictions, axis=-1)
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+
+cm_plot_labels = ['no_side_effects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+model.save('models/model1.h5')
+model.get_weights()
+plt.show()
